0086f186991e819bc0abbbcb540498cd8bcc61e2
FriantAlex <friant.alex@gmail.com>
2016-03-19 18:39:52 -0400

    Movement
    Added speed up, slow down, and stop with continue.

---
+++ ShieldBash/ShieldBash/Assets/Scripts/Mover.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+	
+	public float speed;
+	public float acc;
+	
+	
+	private NavMeshAgent playerAgent;
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+		playerAgent = GameObject.FindWithTag ("Cart").GetComponent<NavMeshAgent> ();
+		
+	}
+
+	
+	void OnTriggerEnter(Collider other) {
+		
+		//speeds up cart 
+		if (other.gameObject.tag == "Cart") {
+
+			Debug.Log ("Boop");
+
+			//spawner start goes here
+			playerAgent.speed = speed;
+			playerAgent.acceleration = acc;
+
+		}
+	}
+}
---
+++ ShieldBash/ShieldBash/Assets/Scripts/Stopper.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stopper : MonoBehaviour {
+
+    public bool isStopped = false;
+
+
+    private NavMeshAgent playerAgent;
+
+
+    // Use this for initialization
+    void Start () {
+	
+		playerAgent = GameObject.FindWithTag ("Cart").GetComponent<NavMeshAgent> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// testing resuming path
+
+		if (isStopped && Input.GetKey (KeyCode.Mouse0)) { //resume if trigger or condition is met
+			isStopped = false;
+			playerAgent.Resume ();
+		}
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+	
+		//stops cart 
+		if (other.gameObject.tag == "Cart") {
+			Debug.Log ("Boop");
+			//spawner start goes here
+			playerAgent.Stop ();
+			isStopped = true;
+		}
+	}
+}
