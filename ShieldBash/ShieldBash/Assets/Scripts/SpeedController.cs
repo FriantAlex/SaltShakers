c66c0db37f40496e88f12b94439e792b8390f4ea
FriantAlex <friant.alex@gmail.com>
2016-03-19 23:17:09 -0400

    Movement
    No more Navmesh, player now rotates in direction of points when moving,
    smoother camera follow, smoother speed up and slow down functions, less
    scripts. Way more control!

---
+++ ShieldBash/ShieldBash/Assets/Scripts/SpeedController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeedController : MonoBehaviour {
+
+	public float maxSpeed;
+	public float acc;
+
+
+	private Patrol playerController;
+
+
+	// Use this for initialization
+	void Start () {
+
+		playerController = GameObject.FindWithTag ("Cart").GetComponent<Patrol> ();
+
+	}
+
+
+	void OnTriggerEnter(Collider other) {
+
+		//speeds up cart 
+		if (other.gameObject.tag == "Cart") {
+			Debug.Log ("Boop");
+			//spawner start goes here
+			playerController.maxPatrolSpeed = maxSpeed;
+			playerController.acceleration = acc;
+
+		}
+	}
+}
