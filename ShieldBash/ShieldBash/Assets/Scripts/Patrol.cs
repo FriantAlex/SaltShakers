a6ae666aadf57e848bfc27307c44f8e52c3362ab
Tyler <Tylerquillen@knights.ucf.edu>
2016-03-20 14:23:28 -0400

    Finished Movement
    Updated movement to be prototype ready.
    Created 4th prefab, stop and start
    Reorganized inspector and scripts

--- ShieldBash/ShieldBash/Assets/Scripts/Patrol.cs
+++ ShieldBash/ShieldBash/Assets/Scripts/Patrol.cs
@@ -4,87 +4,81 @@ using System.Collections;
 public class Patrol : MonoBehaviour {
 
 	public Transform[] waypoints; //list of point
+    [Header ("Movement")]
 	public float maxPatrolSpeed = 6; //max speed
-	public float acceleration; //how fast top speed is reached
-	public float deceleration; // how fast speed of 0 is reached
-	public bool patrolLoop = true; // loops partol
-	public int dampingLook = 4; // rotation speed
-	public float pauseDuration; // stop at point
-	public float speed = 0.0f; // keep constant
+    public float speed = 0.0f; // keep constant
+    public float acceleration; //how fast top speed is reached
 
+    [Header ("Time")]
+    public float pauseDuration; // stop at point
+    public float curTime;
+    public int dampingLook = 4; // rotation speed
 
-	private float curTime;
-	private int currentWayPoint = 0;
-
-	void Awake(){
-	}
-	// Use this for initialization
-	void Start () {
-	
-	}
+    [Header("Track")]
+    public bool patrolLoop = true; // loops partol	
+	public int currentWayPoint = 0;//index of waypoint currently moving towards
+    public bool changing;
 	
 	// Update is called once per frame
 	void Update () {
-
-
-
 	
-		if (currentWayPoint < waypoints.Length) {
+		if (currentWayPoint < waypoints.Length)
+        {
 			PatrolCycle ();
-		} else {
-			if (patrolLoop) {
+		}
+        else
+        {
+			if (patrolLoop)
+            {
 				currentWayPoint = 0;
 			}
 		}
 	}
 
-	void PatrolCycle(){
-
+	void PatrolCycle()
+    {
 		speed = Mathf.Lerp (speed, maxPatrolSpeed, acceleration * Time.deltaTime);
-
 		Vector3 nextWayPoint = waypoints[currentWayPoint].position;
-
 		Vector3 moveDirection = nextWayPoint - transform.position;
 
-		if(moveDirection.magnitude < 1.5){
-			Debug.Log("Enemy is close to nextWayPoint");
-
+		if(moveDirection.magnitude < 1.5)
+        {
+			Debug.Log("Cart is about to change waypoint");          
 			if(curTime == 0)
-				curTime = Time.time;
-			
-			if((Time.time - curTime) >= pauseDuration){
-				Debug.Log("Increasing Waypoint");
-				currentWayPoint++;
-				curTime = 0;
-				}
-			}else{
-				Debug.Log("Reaching in rotation " + moveDirection.magnitude);
+            {
+                curTime = Time.time;              
+            }
 
-				var rot = Quaternion.LookRotation(nextWayPoint - transform.position);
-
-				transform.rotation = Quaternion.Slerp(transform.rotation,rot,Time.deltaTime * dampingLook);
-
-				//charController.Move(moveDirection.normalized * patrolSpeed * Time.deltaTime);
-
-			transform.position = Vector3.MoveTowards (transform.position, nextWayPoint, speed * Time.deltaTime);
-			}
+            if ((Time.time - curTime) >= pauseDuration)
+            {
+                Debug.Log("Increasing Waypoint");
+                currentWayPoint++;
+                curTime = 0;
+            }		
 		}
+        else
+        {
+			Debug.Log("Next waypoint in " + moveDirection.magnitude);
+			var rot = Quaternion.LookRotation(nextWayPoint - transform.position);
+			transform.rotation = Quaternion.Slerp(transform.rotation,rot,Time.deltaTime * dampingLook);
+			//charController.Move(moveDirection.normalized * patrolSpeed * Time.deltaTime);
+			transform.position = Vector3.MoveTowards (transform.position, nextWayPoint, speed * Time.deltaTime);
+		}
+	}
 
 	//draws partol path
-	void OnDrawGizmos(){
-
+	void OnDrawGizmos()
+    {
 		if (waypoints == null)
 			return;
 
-		if (waypoints != null) {
-
-			for (int i = 1; i < waypoints.Length; i++) {
-
+		if (waypoints != null)
+        {
+			for (int i = 1; i < waypoints.Length; i++)
+            {
 				Gizmos.color = Color.green;
 				Gizmos.DrawLine (waypoints [i].position, waypoints [i - 1].position);
 			}
 		}
-
-		}
-
 	}
+}
--- ShieldBash/ShieldBash/Assets/Scripts/SpeedController.cs
+++ ShieldBash/ShieldBash/Assets/Scripts/SpeedController.cs
@@ -3,30 +3,44 @@ using System.Collections;
 
 public class SpeedController : MonoBehaviour {
 
+    public float waitTime;
 	public float maxSpeed;
 	public float acc;
-
 
 	private Patrol playerController;
 
 
 	// Use this for initialization
 	void Start () {
-
 		playerController = GameObject.FindWithTag ("Cart").GetComponent<Patrol> ();
-
 	}
 
 
-	void OnTriggerEnter(Collider other) {
-
-		//speeds up cart 
-		if (other.gameObject.tag == "Cart") {
-			Debug.Log ("Boop");
+	void OnTriggerEnter(Collider other)
+    {   //speeds up cart 
+		if (other.gameObject.tag == "Cart")
+        {
+            Debug.Log(this.gameObject.name);
+            if (this.gameObject.name == "StopperStarter")
+            {
+                Debug.Log("StartStop engaged");
+                StartCoroutine(StartStop());
+                return;
+            }
+            Debug.Log ("Boop");
 			//spawner start goes here
 			playerController.maxPatrolSpeed = maxSpeed;
 			playerController.acceleration = acc;
-
 		}
 	}
+
+    private IEnumerator StartStop()
+    {
+        float curSpeed = playerController.maxPatrolSpeed;//players current max speed while en route
+        float curAcc = playerController.acceleration;//players current acceleration while en route
+        playerController.speed = maxSpeed;
+        playerController.acceleration = acc;
+        yield return new WaitForSeconds(waitTime);
+        playerController.acceleration = curAcc;
+    }
 }
