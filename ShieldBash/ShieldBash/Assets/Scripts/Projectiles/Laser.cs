d25911a143646b369bc7534206824e2b4086e83b
FriantAlex <friant.alex@gmail.com>
2016-03-20 21:49:34 -0400

    Demo Staging
    Added new scene "Projectile Staging" Press MB1 to fire bouncing
    projectiles, and press SPACE to fire laser. The player is controlled by
    mouse position.

---
+++ ShieldBash/ShieldBash/Assets/Scripts/Projectiles/Laser.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Laser : MonoBehaviour {
+
+	public Transform startPoint;
+	public Transform endPoint;
+
+
+	private LineRenderer laserLine;
+
+	// Use this for initialization
+	void Start () {
+	
+		laserLine = GetComponent<LineRenderer> ();
+		laserLine.SetWidth (8, 8);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		laserLine.SetPosition (0, startPoint.position);
+		laserLine.SetPosition (1, endPoint.position);
+
+	}
+}
